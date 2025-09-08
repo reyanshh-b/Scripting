11f5631bde51393f928696bdf10ac4284465f817
Reyansh Bansal <coolreybansal@gmail.com>
2025-09-08 13:09:20 +0000

    idk what i changed

--- cpp/game.cpp
+++ cpp/game.cpp
@@ -12,11 +12,15 @@ using namespace std;
 class User {
 public:
     int fuel;
+    atomic<bool> travelerEventPending = false;
+    string travelerPrompt;
+    int travelerRandomChance = 0;
 
-    User(int initial_fuel) : fuel(initial_fuel), running(false) {}
+    User(int initial_fuel) : fuel(initial_fuel), running(false), randEventRunning(false) {}
 
     ~User() {
         stopFuelThread();
+        stopRandEventThread();
     }
 
     void startFuelThread() {
@@ -33,19 +37,31 @@ public:
         }
     }
 
+    void startRandEventThread(){
+        if (randEventThread.joinable()) return; // already running
+        cout << "Random event thread started." << endl;
+        randEventRunning = true;
+        randEventThread = thread(&User::randEventTread, this);
+    }
+
+    void stopRandEventThread(){
+        randEventRunning = false;
+        if (randEventThread.joinable()) {
+            randEventThread.join();
+        }
+    }
+
 private:
     thread fuelThread;
+    thread randEventThread;
     atomic<bool> running;
+    atomic<bool> randEventRunning;
 
     void consumeFuel() {
         while (running) {
             if (fuel > 0) {
                 fuel--;
                 cout << fuel << " units of fuel remaining." << endl;
-                cout << "\033[2K"; // Clear line
-                cout << "\033[1A"; // Move cursor up
-                cout << "\033[2K"; // Clear line again
-
             } else {
                 cout << "Out of fuel!" << std::endl;
                 cout << "\033[2K"; // Clear line
@@ -55,10 +71,59 @@ private:
             this_thread::sleep_for(chrono::seconds(1));
         }
     }
+
+    void randEventTread(){
+    while(randEventRunning){
+        if(rand() % 100 < 30) { // 30% chance for a random event
+            cout << "\033[31m" << "A random event has occurred!" << "\033[32m" << endl;
+            const int randEvent = rand() % 5; //numbers 0-4
+            switch(randEvent){
+                case 0:
+                    cout << "\033[34m" << "GAS LEAK! You lose 10 fuel!" << "\033[32m" << endl;
+                    fuel -= 10;
+                    if(fuel < 0){
+                        fuel = 0;
+                    }
+                    break;
+                case 1:
+                    cout << "\033[34m" << "You found a fuel barrel! +10 fuel" << "\033[32m" << endl;
+                    fuel += 10;
+                    if(fuel > 100){
+                        fuel = 100;
+                    }
+                    break;
+                case 2: {
+                    travelerEventPending = true;
+                    travelerPrompt = "\033[34mA traveler finds you and asks if you can give them some fuel [yes/no]\033[32m";
+                    travelerRandomChance = (rand() % 2) + 1;
+                    break;
+                }
+                case 3: {
+                    const int randomSuperRareChance = rand() % 100;
+                    if(randomSuperRareChance < 1){
+                        cout << "\033[31m" << "A tree falls on your car and hits you square on the head, GAME OVER!" << "\033[32m" << endl;
+                    }
+                    break;
+                }
+                case 4:
+                    cout << "\033[34m" << "You found a hidden fuel stash! +50 fuel" << "\033[32m" << endl;
+                    fuel += 50;
+                    if(fuel > 100){
+                        fuel = 100;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        this_thread::sleep_for(chrono::seconds(5));
+    }
+}
 };
 
 
 int main(){
+    srand(time(nullptr));
     system("clear");
     cout << "\033[32m" << endl; // Green output text
     vector<string> actions = {
@@ -67,7 +132,6 @@ int main(){
         "look for oil",
         "list"
     };
-    //create a car object
     User user(100);
 
     //color variables
@@ -77,92 +141,43 @@ int main(){
     const string blue = "\033[34m";
     while(true){
         string action;
+            if (user.travelerEventPending) {
+            cout << user.travelerPrompt << endl; // Show prompt first
+            getline(cin, action);                // Get user input
+            transform(action.begin(), action.end(), action.begin(), ::tolower);
+
+            if(user.travelerRandomChance == 1){
+                if(action == "yes"){
+                    cout << blue << "The traveler thanks you and gives you a map to a hidden fuel stash! +20 fuel" << reset << endl;
+                    user.fuel += 20;
+                    if(user.fuel > 100) user.fuel = 100;
+                }else if (action == "no"){
+                    cout << blue << "The traveller curses you and you lost half your fuel!" << reset << endl;
+                    user.fuel /= 2;
+                    if(user.fuel < 0) user.fuel = 0;
+                }
+            }else{
+                if(action == "yes"){
+                    cout << blue << "The traveler turns out to be a bandit and steals a quarter of your fuel!" << reset << endl;
+                    user.fuel -= user.fuel / 4;
+                    if(user.fuel < 0) user.fuel = 0;
+                }else if (action == "no"){
+                    cout << blue << "The traveler thanks you graciously and gives you a magic fuel can! No fuel is lost for 15 seconds!" << reset << endl;
+                    user.stopFuelThread();
+                    this_thread::sleep_for(chrono::seconds(15));
+                    user.startFuelThread();
+                }
+            }
+            user.travelerEventPending = false;
+            continue; // skip normal action prompt
+        }   
         cout << "Enter an action (list to see all actions): " << endl;
         getline(cin, action);
-
-        if(rand() % 100 < 50){
-            cout << red << "A random event has occurred!" << reset << endl;
-            const int randEvent = rand() % 5; //numbers 0-4
-            switch(randEvent){
-                case 0:
-                    cout << blue << "GAS LEAK! You lose 10 fuel!" << reset << endl;
-                    user.fuel -= 10;
-                    if(user.fuel < 0){
-                        user.fuel = 0;
-                    }
-                    break;
-                case 1:
-                    cout << blue << "You found a fuel barrel! +10 fuel" << reset << endl;
-                    user.fuel += 10;
-                    if(user.fuel > 100){
-                        user.fuel = 100;
-                    }
-                    break;
-                case 2: {
-                    string userInput;
-                    cout << blue << "A traveler finds you and asks if you can give them some fuel [yes/no]" << reset << endl;
-                    getline(cin, userInput);
-                    const int randomChance = (rand() % 2) + 1; // numbers 1-2
-                    transform(userInput.begin(), userInput.end(), userInput.begin(), ::tolower);
-                    switch(randomChance){
-                        case 1:
-                            if(userInput == "yes"){
-                                cout << blue << "The traveler thanks you and gives you a map to a hidden fuel stash! +20 fuel" << reset << endl;
-                                user.fuel += 20;
-                                if(user.fuel > 100){
-                                    user.fuel = 100;
-                                }
-                            }else if (userInput == "no"){
-                                cout << blue << "The traveller curses you and you lost half your fuel!" << reset << endl;
-                                user.fuel /= 2;
-                                if(user.fuel < 0){
-                                    user.fuel = 0;
-                                }
-                            }
-                            break;
-                        case 2:
-                            if(userInput == "yes"){
-                                cout << blue << "The traveler turns out to be a bandit and steals a quarter of your fuel!" << reset << endl;
-                                user.fuel -= user.fuel / 4;
-                                if(user.fuel < 0){
-                                    user.fuel = 0;
-                                }
-                            }else if (userInput == "no"){
-                                cout << blue << "The traveler thanks you graciously and gives you a magic fuel can! No fuel is lost for 15 seconds!" << reset << endl;
-                                user.stopFuelThread();
-                                this_thread::sleep_for(chrono::seconds(15));
-                                user.startFuelThread();
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                }
-                case 3: {
-                    const int randomSuperRareChance = rand() % 100;
-                    if(randomSuperRareChance < 0.25){
-                        cout << red << "A tree falls on your car and hits you square on the head, GAME OVER!" << reset << endl;
-                    }
-                    break;
-                }
-                case 4:
-                    cout << blue << "You found a hidden fuel stash! +50 fuel" << reset << endl;
-                    user.fuel += 50;
-                    if(user.fuel > 100){
-                        user.fuel = 100;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
 
         for(const auto& act : actions){
             if(action == act){
                 if (action == "list"){
                     system("clear");
-                    //list all actions
                     cout << "Available actions:" << endl;
                     for(const auto& a : actions){
                         cout << "- " << a << endl;
@@ -171,13 +186,14 @@ int main(){
                     system("clear");
                     cout << red << "car sputters for a little but then runs" << reset << endl;
                     user.startFuelThread();
+                    user.startRandEventThread();
                 }else if( action == "turn off car"){
                     system("clear");
                     cout << red << "car shuts off" << reset << endl;
                     user.stopFuelThread();
+                    user.stopRandEventThread();
                 }else if(action == "look for oil"){
                     system("clear");
-                    //30% chance to find oil
                     int chance = rand() % 100;
                     if(chance < 30){
                         cout << yellow << "You found some oil!" << reset << endl;
@@ -187,6 +203,7 @@ int main(){
                     }
                 }else if(action == "exit"){
                     user.stopFuelThread();
+                    user.stopRandEventThread();
                     return 0;
                 }
             }
